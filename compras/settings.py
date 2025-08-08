7a964fb4b6e4b0ba384bf659f59b805a4a97fa0a
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2025-08-08 19:04:16 -0400

    Update settings.py

--- compras/settings.py
+++ compras/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2agi9gtlc5kf^pi1mw3w7r&s!-h1!d-2(u4$ultw8ikspwk_)g"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if DEBUG:
+    SECRET_KEY = "django-insecure-2agi9gtlc5kf^pi1mw3w7r&s!-h1!d-2(u4$ultw8ikspwk_)g"
+else:
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
